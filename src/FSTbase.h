6b320bc595e4539dc8eac1bbefe0b15fbee77994
ARmodRey <armodrej01@gmail.com>
2020-09-30 08:13:25 +0300

    add func is_folder and is_dir

--- src/FSTbase.h
+++ src/FSTbase.h
@@ -36,7 +36,7 @@ namespace FSTool {
         virtual std::string back() = 0;             // return last element
         virtual bool range(int index) = 0;          // check index
         virtual strvect get_elements_of_path() = 0; // return elements of path
-        virtual void update() = 0;                  // update inkformation of object 
+        virtual void update() = 0;                  // update information of object 
 
         //return first element 
         std::string front(){ 
