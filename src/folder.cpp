f2dd6b36526b5cbd483ba4c01714fcf9c447e93b
ARmodRey <armodrej01@gmail.com>
2021-01-06 22:18:51 +0300

    putting things in order in folder.cpp

--- src/folder.cpp
+++ src/folder.cpp
@@ -1,63 +1,5 @@
 #include "folder.h"
 
-// FSTool::_dirinfo::_dirinfo(std::string full_name){
-//     this->full_name = full_name;
-//     int *find = new int(this->full_name.find_last_of("/\\")); // split into folder name and path
-//     this->path = this->full_name.substr(0, *find);            // set path
-//     this->name = this->full_name.substr(*find + 1);           // set folder name
-//     delete find;    
-// #ifdef WIN32
-// 	if (_access(full_name.c_str(), 0))
-// #elif defined(unix) 
-//     if (access(full_name.c_str(), 0) !=0)
-// #endif
-//     {
-// 		return; // if folder not exists 
-//     }
-// #ifdef WIN32
-//     struct _finddata_t data;
-// 	intptr_t done = _findfirst(this->full_name.c_str(), &data);
-//     this->size += data.size;
-// 	while (_findnext(done, &data) == 0) {
-//         this->length++;
-// 		if (data.attrib == _A_SUBDIR && this->_elements != 0) {
-// 			this->size += dir_information(full_name).size;
-//             this->folders++;
-//             this->folders += dir_information(full_name).folders;
-//             this->files += dir_information(full_name).files;
-// 		}else{
-//             this->files++;
-//             this->size += data.size;
-//         }
-// 	}
-// 	_findclose(done);
-// #elif defined (unix)
-//     DIR *dir = opendir(this->full_name.c_str());
-// 	struct dirent *ent;
-//     struct stat data;
-//     stat(full_name.c_str(),&data);
-//     while((ent = readdir(dir)) != NULL){
-//         this->length++;
-// 	    if(ent->d_type == DT_DIR){
-//             if ( strcmp( ".", ent->d_name ) == 0 || strcmp( "..", ent->d_name ) == 0 ){
-// 			    continue;
-//             }
-// 			this->size += dir_information((full_name + "/" + ent->d_name).c_str()).size;
-//             this->folders++;
-//             this->folders += dir_information((full_name + "/" + ent->d_name).c_str()).folders;
-//             this->files += dir_information((full_name + "/" + ent->d_name).c_str()).files;
-// 		}
-//         if(ent->d_type == DT_REG){
-// 			stat((full_name + "/" + ent->d_name).c_str(),&data);
-//             this->files++;
-// 		    this->size += data.st_size;
-//         }
-// 	}
-// 	closedir(dir);
-// #endif
-//     this->elements += this->files + this->folders;// count elements
-// }
-
 FSTool::folder::folder(std::string name) : FSTool::_base(name) {
     if (this->exists()){
         this->update();
@@ -302,7 +244,7 @@ FSTool::strvect FSTool::folder::get_content_list(){
 
 int FSTool::folder::find(std::string object, int begin, int end){
     if(!this->exists()){
-        throw fs_exception("file not found", -2); // if file exists
+        throw fs_exception("folder not found", -2); // if file exists
     }
     static int _find;
     static int _begin; // begin position
